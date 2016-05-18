0c979b4a5000dc6a6fa0edb9a18395d0f647e0ee
fengshikun <fengxiaoxiaokun@gmail.com>
2016-05-18 20:09:16 +0800

    change eval_metrics

--- train_model.py
+++ train_model.py
@@ -78,10 +78,7 @@ def fit(args, network, data_loader, batch_end_callback=None):
         initializer        = mx.init.Xavier(factor_type="in", magnitude=2.34),
         **model_args)
 
-    eval_metrics = ['accuracy']
-    ## TopKAccuracy only allows top_k > 1
-    for top_k in [5, 10, 20]:
-        eval_metrics.append(mx.metric.create('top_k_accuracy', top_k = top_k))
+    eval_metrics = ['crossEntropy']
 
     if batch_end_callback is not None:
         if not isinstance(batch_end_callback, list):
